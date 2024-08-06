b9113681f0a666085fcf10fce0bd2c12ff68f655
Krisa Duwal <duwalkrisha35@gmail.com>
2024-08-06 12:54:12 +0545

    drag and drop

--- SeleniumPytest/demo/dragAndDrop.py
+++ SeleniumPytest/demo/dragAndDrop.py
@@ -1,19 +1,48 @@
-#mileko chaina.. k
+import time
+import unittest
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.geeksforgeeks.org/")
 
-sourceElement = driver.find_element(By.LINK_TEXT, "Courses")
-TargetElement = driver.find_element(By.LINK_TEXT, "Hard")
+class DragTest(unittest.TestCase):
 
-#action chain object
-action = ActionChains(driver)
+    def setUp(self):
+        self.driver = webdriver.Chrome()
 
-#drag and drop item
-action.drag_and_drop(sourceElement, TargetElement)
+    def test_drag_drop(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get('https://jqueryui.com/draggable/')
+        driver.switch_to.frame(0)
 
-action.perform()
+        # find source element
+        sourceElement = driver.find_element(By.ID, 'draggable')
+
+
+        action = ActionChains(driver)
+        action.drag_and_drop_by_offset(sourceElement, 100, 100)
+        action.perform()
+
+        time.sleep(5)
+
+        driver.get('https://jqueryui.com/droppable/')
+        driver.switch_to.frame(0)
+
+        sourceElement = driver.find_element(By.ID, 'draggable')
+        targetElement = driver.find_element(By.ID, 'droppable')
+
+        action2 = ActionChains(driver)
+        action2.drag_and_drop(sourceElement, targetElement).perform()
+        time.sleep(6)
+
+        self.assertEqual('Dropped!', targetElement.text)
+        time.sleep(5)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
